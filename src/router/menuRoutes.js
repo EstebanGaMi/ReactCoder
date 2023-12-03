461f7feb6460d6fe9be8de88b8a5f8fffee186ec
EstebanGaMi <stevegaray1234@gmail.com>
2023-12-03 09:02:13 +0000

    detailrute

--- src/router/menuRoutes.js
+++ src/router/menuRoutes.js
@@ -1,9 +1,7 @@
 import Cart from "../components/pages/cart/Cart";
-import Checkout from "../components/pages/checkout/Checkout";
-import CheckoutFormik from "../components/pages/checkoutFormik/CheckoutFormik";
 import CheckoutOficial from "../components/pages/checkoutOficial/CheckoutOficial";
 import ItemDetailContainer from "../components/pages/itemDetailContainer/ItemDetailContainer";
-import ItemListContainer from "../components/pages/itemListContainer/ItemListContainer";
+import ItemListContainer from "@components/pages/itemListContainer/ItemListContainer";
 
 export const routes = [
   {
